609747288aaea76ff43af69a44eb9c2967fa67bf
Lukasz <Lukasz Reszke>
2017-03-13 22:15:56 +0100

    Change ViewOpearation to ViewGenerators

--- IntervalsXamarinApp/IntervalsXamarinApp/IntervalsXamarinApp/MainPage.xaml.cs
+++ IntervalsXamarinApp/IntervalsXamarinApp/IntervalsXamarinApp/MainPage.xaml.cs
@@ -20,21 +20,21 @@ namespace IntervalsXamarinApp
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = @"http://lorempixel.com/300/200/people/4" }},
                  new Exercise { Name = "smierc", Description = "uciekaj", TimeLenght = TimeSpan.FromMinutes(80),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = $"http://lorempixel.com/300/200/people/3" }},
-                 new Exercise { Name = "hardkor", Description = "nie dasz rady", TimeLenght = TimeSpan.FromMinutes(80),
+                 new Exercise { Name = "jazda", Description = "nie dasz rady", TimeLenght = TimeSpan.FromMinutes(80),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = @"http://lorempixel.com/300/200/people/2" }},
-                 new Exercise { Name = "masakra", Description = "spocisz sie", TimeLenght = TimeSpan.FromMinutes(60),
+                 new Exercise { Name = "trojkat", Description = "spocisz sie", TimeLenght = TimeSpan.FromMinutes(60),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = @"http://lorempixel.com/300/200/people/5" }},
-                 new Exercise { Name = "smierc", Description = "uciekaj", TimeLenght = TimeSpan.FromMinutes(80),
+                 new Exercise { Name = "gwiazda", Description = "uciekaj", TimeLenght = TimeSpan.FromMinutes(80),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = $"http://lorempixel.com/300/200/people/6" }},
-                 new Exercise { Name = "hardkor", Description = "nie dasz rady", TimeLenght = TimeSpan.FromMinutes(80),
+                 new Exercise { Name = "Moze kardio?", Description = "nie dasz rady", TimeLenght = TimeSpan.FromMinutes(80),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = @"http://lorempixel.com/300/200/people/7" }},
-                 new Exercise { Name = "masakra", Description = "spocisz sie", TimeLenght = TimeSpan.FromMinutes(60),
+                 new Exercise { Name = "Klatka i biceps", Description = "spocisz sie", TimeLenght = TimeSpan.FromMinutes(60),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = @"http://lorempixel.com/300/200/people/8" }},
-                 new Exercise { Name = "smierc", Description = "uciekaj", TimeLenght = TimeSpan.FromMinutes(80),
+                 new Exercise { Name = "Po co komu radio?", Description = "uciekaj", TimeLenght = TimeSpan.FromMinutes(80),
                      ExImage = new ExerciseImage { Height = 50, Width = 50, UrlAdress = $"http://lorempixel.com/300/200/people/9" }}
             };
-            IViewGenerator generate = new StackViewGenerator(stack);
-            generate.GenerateView(exerciseList);
+            IViewGenerator exerciseListView = new StackViewGenerator(stack);
+            exerciseListView.GenerateView(exerciseList);
      
             //var tapRecognizer = new TapGestureRecognizer();
             //tapRecognizer.Tapped += (s, e) => { label.Text = "zmieniono"; };
